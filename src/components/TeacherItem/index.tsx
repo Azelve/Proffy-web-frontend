1b46d637361553a491a70b59707ea7c25542b70a
Rodrigo Camilo <43912333+Azelve@users.noreply.github.com>
2020-08-06 18:28:41 -0300

    Updated

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,51 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+  };
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return(
     <article className="teacher-item">
           <header>
-            <img src="https://pbs.twimg.com/profile_images/1290293163802988544/R8hcilJv_400x400.jpg" alt="Rodrigo Camilo"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-              <strong>Rodrigo Camilo</strong>
-              <span>Inglês</span>
+              <strong>{teacher.name}</strong>
+              <span>{teacher.subject}</span>
             </div>
           </header>
 
-          <p>
-            Entusiasta em ensinar e aprender com o aluno.
-            <br/>
-            Sempre buscando inovar e ter dinheiro para gastar com o amor da vida.
-          </p>
+          <p>{teacher.bio}</p>
 
           <footer>
             <p>
               Preço/hora
-              <strong>R$ 80,00</strong>
+              <strong>R$ {teacher.cost}</strong>
             </p>
-            <button type="button">
+            <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
               <img src={whatsappIcon} alt="Whatsapp"/>
               Entrar em contato
-            </button>
+            </a>
           </footer>
         </article>
   )
